7e567ce0361e4f1727905dcb73f44fdaede2f9a6
15105103 <15105103@W1846666.portoalegre.pucrsnet.br>
2016-06-02 17:13:15 -0300

    Métodos Retirados. Primeiro fazer UML, depois implementar

--- src/Dominio/Parquimetro.java
+++ src/Dominio/Parquimetro.java
@@ -1,63 +1,33 @@
 package Dominio;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 
 
 public class Parquimetro {
-    private int identification;
-    private String addres;
+    private final String identification;
+    private final String addres;
     private List<Ticket> tickets;
-    private final LocalTime inicioTarifa;
-    private final LocalTime fimTarifa;
-    private final LocalTime tempoMinimo;
-    private final LocalTime tempoMaximo;
-    private final LocalTime incremento;
-    private final double valorMinimo;
-    private final double valorMaximo;
-    private final double valorIncremento;
+    private final LocalDateTime inicioTarifa = LocalDateTime.of(LocalDate.now(), LocalTime.of(8, 30));
+    private final LocalDateTime fimTarifa = LocalDateTime.of(LocalDate.now(), LocalTime.of(18, 30));
+    private final LocalTime tempoMinimo = LocalTime.of(0, 30);
+    private final LocalTime tempoMaximo = LocalTime.of(2, 00);
+    private final LocalTime incremento = LocalTime.of(0, 10);
+    private final double valorMinimo = 0.75;
+    private final double valorMaximo = 3.0;
+    private final double valorIncremento = 0.25;
     private double valorPago;
     
     
-    public Parquimetro(int newId, String newAddres, LocalTime tempoMinimo, LocalTime tempoMaximo,
-                        double valorMinimo, double valorMaximo, LocalTime incremento, double valorIncremento,
-                        LocalTime inicioTarifa, LocalTime fimTarifa){
-        if(String.valueOf(newId).length() == 5) identification = newId;
-        this.inicioTarifa = inicioTarifa;
-        this.fimTarifa = fimTarifa;
-        this.tempoMinimo = tempoMinimo;
-        this.tempoMaximo = tempoMaximo;
-        this.valorMinimo = valorMinimo;
-        this.valorMaximo = valorMaximo;
-        this.incremento = incremento;
-        this.valorIncremento = valorIncremento;
+    public Parquimetro(String newId, String newAddres) throws ParquimetroDAOException{
+        if(newId.length() == 5) identification = newId;
+        else throw new ParquimetroDAOException("Formato Inválido");
         addres = newAddres;
-        
     }
     
     public void geraTicket(LocalTime saida, double valorPago){
-        if(isTarifying() == false){
-            Ticket t = new Ticket(0,saida,identification,addres);
-            return;
-        }
-        
-        /*
-            Se o tempo desejado exceder o tempo máximo
-        */
-        if(diferencaTempo(saida) > 120 || diferencaTempo(saida) < 30) return;
-        
-        /*
-            Verifica se o valor pago condiz com o tempo de estadia
-        */
-        int diferencaTempo = diferencaTempo(saida);
-        if(!(verificaValorPago(diferencaTempo,valorPago))) return;
-        
-        /*
-            Se tempo de estadia e valor pago estão ok, cria o ticket
-        */
-        
-        Ticket t = new Ticket(valorPago, saida, identification, addres);
-        t.imprimeTicket();
         
     }
     
@@ -65,50 +35,26 @@ public class Parquimetro {
         Verifica se o valor pago condiz com o tempo de estadia escolhido
     */
     private boolean verificaValorPago(int diferencaTempo, double valorPago){
-        
-        double valorTotal = valorMinimo;
-        int tempoTotal = tempoMinimo.getMinute();
-        while(tempoTotal < diferencaTempo){
-            valorTotal += valorIncremento;
-            tempoTotal += 10;
-        }
-        
-        if(valorTotal == valorPago) return true;
         return false;
-        
     }
     
     /*
         Retorna a diferença de tempo em minutos entre a chegada e a hora de saida
     */
     private int diferencaTempo(LocalTime saida){
-        
-        int hora = saida.getHour() * 60;
-        int minuto = saida.getMinute();
-        int tempoTotalSaida = hora+minuto;
-        
-        
-        hora = LocalTime.now().getHour() * 60;
-        minuto = LocalTime.now().getMinute();
-        int tempoTotalAgora = hora+minuto;
-        
-        return tempoTotalSaida - tempoTotalAgora;  
-        
+        return -1;
     }
-    
-    public int getIdentification(){ return identification; }
-    public String getAddres(){ return addres; }
-    public List getTickets(){ return tickets; }
     
     /*
         Verifca se o parquimetro está tarifando no horário atual
     */
     private boolean isTarifying(){
-        return LocalTime.now().isBefore(fimTarifa) && LocalTime.now().isAfter(fimTarifa);
+        return false;
     }
     
-    public void setId(int newId){ identification = newId; }
-    public void setAddres(String newAddres){ addres = newAddres; }
+    public String getIdentification(){ return identification; }
+    public String getAddres(){ return addres; }
+    public List getTickets(){ return tickets; }
     
 
 }
