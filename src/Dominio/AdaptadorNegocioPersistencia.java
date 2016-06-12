67c143494bcb1b0c79a867d452896bd652526d43
Vinicius <vinicius.s.sesti@gmail.com>
2016-06-11 21:01:09 -0300

    Ortografia xisde.

--- src/Dominio/AdaptadorNegocioPersistencia.java
+++ src/Dominio/AdaptadorNegocioPersistencia.java
@@ -18,7 +18,7 @@ public class AdaptadorNegocioPersistencia {
     public void armazenaTicket(Ticket t, IPagamento p, double valorPago) throws RelatorioDAOException {
         
         TicketDTO ticket = new TicketDTO(t.getSerial(),t.getChegada(),t.getSaida(),
-                                        t.getParqId(), t.getParqAddres(),p.getTipo(),
+                                        t.getParqId(), t.getParqAddress(),p.getTipo(),
                                         valorPago);
         relatorio.armazenaTicket(ticket);
         
